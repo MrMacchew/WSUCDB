2d3a035ca791d141b63fa9e4dee7e846f14026b5
MrMacchew <mattcain@Matts-MacBook-Pro.local>
2015-08-08 00:21:58 -0600

    Edits to CSS and Jquery.

--- app/assets/javascripts/myapplication.js
+++ app/assets/javascripts/myapplication.js
@@ -1,5 +1,22 @@
 $(window).load( function() {  
 	$('div.CDB_TH_NAME').each(function(){
-					$(this).css('width', $(this).siblings('img').width());
+		$(this).css('width', $(this).siblings('img').width());
+		$(this).parent().css('width', $(this).siblings('img').width());
+	});
+	
+	/*$('div.wrapper').each(function(){
+		$(this).css('height', $(this).child('img').height());
+	});
+	*/
+	$('div.wrapper').hover(function(){
+		$(this).children('.CDB_TH_NAME').stop().slideToggle('fast');
+	}, function(){
+		$(this).children('.CDB_TH_NAME').stop().slideToggle('fast');
+	});
+});
+
+$(window).resize(function(){
+	$('div.CDB_TH_NAME').each(function(){
+		$(this).css('width', $(this).siblings('img').width());
 	});
 });
