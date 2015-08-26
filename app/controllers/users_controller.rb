1b8f6dd0170717028b94b46a5c6fdfad0ded82db
MrMacchew <Macchew2@Gmail.com>
2015-08-26 12:44:44 -0600

    Major adjustments to the show view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,15 +19,31 @@ class UsersController < ApplicationController
 	def index
 		@users = User.all.order("last_name")
 	end
+	
+	def edit
+		@user = User.find(params[:id])		
+	end
 
-  def destroy
-		@user = User.find(params[:id])
+    def destroy
+	 	@user = User.find(params[:id])
 		@user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+      respond_to do |format|
+        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
-  end
+	
+	def update
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+	  end
+	end
 
 	private
 		def user_params
